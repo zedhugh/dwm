53e556cb88de31261e83952d7c129529a67355b4
Zedhugh Chen <zedhugh@gmail.com>
2022-12-06 00:28:29 +0800

    add dir local variable "whitespace-style"

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((c-mode . ((eval . (whitespace-mode 1)))))
+((c-mode . ((whitespace-style . (tabs space-after-tab tab-mark))
+            (eval . (whitespace-mode 1)))))
